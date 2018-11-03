a38b8a15c1c78a114bb538f33421d04540c3ac9e
James <42484571+smitteh1@users.noreply.github.com>
2018-11-02 21:23:24 -0400

    Added Most of reaction listener

--- src/com/bot/reactions/ReactionListener.java
+++ src/com/bot/reactions/ReactionListener.java
@@ -1,5 +1,37 @@
 package com.bot.reactions;
 
-public class ReactionListener {
+import net.dv8tion.jda.core.entities.MessageReaction;
+import net.dv8tion.jda.core.entities.MessageReaction.ReactionEmote;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.react.GenericMessageReactionEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+public class ReactionListener extends ListenerAdapter {
+
+	boolean answered = false;
+	String user;
+	
+	public ReactionListener(String userid) {
+		user = userid;
+		System.out.println("reg");
+	}
+	
+	@Override
+	public void onMessageReactionAdd(MessageReactionAddEvent event) {
+		String eMessageId = event.getMessageId();
+		MessageReaction reaction = event.getReaction();
+		ReactionEmote emote = reaction.getReactionEmote();
+		User auth = event.getUser();
+		boolean isBot = auth.isBot();
+		
+		if(!answered) {
+			if(!isBot) {
+				if(auth.getId().equals(user)) {
+					System.out.println("WTF");
+				}
+			}
+		}
+	}
+	
 }
