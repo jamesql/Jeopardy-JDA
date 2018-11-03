ec494081f221032a12d56ee07091f945ae91df92
James <42484571+smitteh1@users.noreply.github.com>
2018-11-03 18:55:56 -0400

    Update DBC.java

--- src/com/bot/database/DBC.java
+++ src/com/bot/database/DBC.java
@@ -1,21 +1,38 @@
 package com.bot.database;
 
+import java.awt.Color;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageChannel;
+
 public class DBC {
 
 		public String userid;
 		public int level;
 		public int correct;
+		private MessageChannel channel;
+		public String[][] arr = new String[10][2];
+		public int rank;
 		
 		public DBC(String user) throws Exception {
 			userid = user;
 			level = getLevel();
 			correct = getCorrect();
+			
+		}
+		
+		EmbedBuilder eb = new EmbedBuilder();
+		
+		public DBC(String user, MessageChannel chan) throws Exception {
+			userid = user;
+			level = getLevel();
+			correct = getCorrect();
+			channel = chan;
 			
 		}
 	
@@ -63,12 +80,61 @@ public class DBC {
 			 Connection conn = getConnection();
 			 PreparedStatement state = conn.prepareStatement("INSERT INTO level (level,correct,userid) VALUES (0,0," + userid + ")");
 			 state.execute();
+			 conn.close();
 		 }
 		 
 		 public void addCorrect() throws Exception {
 			 Connection conn = getConnection();
 			 PreparedStatement state = conn.prepareStatement("UPDATE level SET correct=" + (correct + 1) + " WHERE userid = '" + userid + "';");
 			 state.execute();
+			 if (correct % 15 == 0) {
+				 addLevel();
+					eb.setTitle("Jeopardy Rank Up!");
+					eb.addField("Level Up!", "You are now level " + (level + 1), false);
+					eb.setFooter("Get Thinking!", null);
+					eb.setColor(Color.CYAN);
+					
+					channel.sendMessage(eb.build()).queue(m ->{
+						m.addReaction("\uD83C\uDF89").queue();
+					});
+			 }
+			 conn.close();
 		 }
-	
+		 
+		 public void addLevel() throws Exception {
+			 Connection conn = getConnection();
+			 PreparedStatement state = conn.prepareStatement("UPDATE level SET level=" + (level + 1) + " WHERE userid = '" + userid + "';");
+			 state.execute();
+			 conn.close();
+		 }
+		 
+		 public void getLb() throws Exception {
+			 Connection conn = getConnection();
+			 PreparedStatement state = conn.prepareStatement("SELECT correct,userid FROM level ORDER BY -correct");
+			 ResultSet res = state.executeQuery();
+			 res.beforeFirst();
+			 res.next();
+			 arr[0][0] = res.getString("userid");
+			 arr[0][1] = Integer.toString(res.getInt("correct"));
+			 System.out.println(res.getString("userid") + " : " + res.getInt("correct"));
+			 for(int x = 0; x < 5; x++) {
+				 res.next();
+				 arr[x][0] = res.getString("userid");
+				 arr[x][1] = Integer.toString(res.getInt("correct"));
+				 System.out.println(res.getString("userid") + " : " + res.getInt("correct"));
+			 }
+			 res.first();
+			 rank = 1;
+			 String currentId = res.getString("userid");
+			 while(currentId != userid) {
+				res.next();
+				if (res.isAfterLast())  {
+						rank = 1;
+						currentId = userid;
+				}else{
+				currentId = res.getString("userid");
+				rank++;
+			 }
+		 } 
+	}
 }
