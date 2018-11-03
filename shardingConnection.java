f5e9bf7314d1b024ddbfc50603316bb98d33c577
James <42484571+smitteh1@users.noreply.github.com>
2018-11-03 13:05:01 -0400

    Added GuildListener

--- shardingConnection.java
+++ shardingConnection.java
@@ -5,6 +5,7 @@ import javax.security.auth.login.LoginException;
 import com.bot.api.getQ;
 import com.bot.commands.CommandListener;
 import com.bot.reactions.ReactionListener;
+import com.not.bfd.GuidListener;
 
 import argo.saj.InvalidSyntaxException;
 import net.dv8tion.jda.core.entities.Game;
@@ -23,11 +24,11 @@ import net.dv8tion.jda.core.hooks.InterfacedEventManager;
 public class connection {
 	
 	final static Game game = Game.playing("Get Thinking! (Beta)");
-	final static String token = "///";
+	final static String token = "NTA3NzI1MjU5NjE3NTk5NTA5.Dr9W2g.XNrkea3JRjq_hy1zDuSwxAv67pU";
 
     public static void main(String[] args) throws LoginException, IOException, InvalidSyntaxException {
         new DefaultShardManagerBuilder()
-        		.addEventListeners(new CommandListener())
+        		.addEventListeners(new CommandListener(), new GuidListener())
         		.setGame(game)
         		.setToken(token)
         		.build();
