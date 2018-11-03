14d345ccfb29bb6a922151075c0d789dfa08c892
James <42484571+smitteh1@users.noreply.github.com>
2018-11-03 13:09:16 -0400

    Started Guild Listener

--- src/com/bot/guilds/GuildListener.java
+++ src/com/bot/guilds/GuildListener.java
@@ -1,5 +1,21 @@
 package com.bot.guilds;
 
-public class GuildListener {
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+public class GuildListener extends ListenerAdapter {
+
+	private String apitoken = "";
+	private String botId = "";
+	
+	
+	
+	@Override
+	public void onGuildJoin(GuildJoinEvent event) {
+		JDA bot = event.getJDA(); 
+		int serverCount  = bot.getGuilds().size();
+		String guildName = event.getGuild().getName();
+		System.out.println("Guild Added : " + guildName);
+	}
 }
