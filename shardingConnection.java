5b221a78de2267071002cedef94981c85c43d69a
James <42484571+smitteh1@users.noreply.github.com>
2018-11-03 13:07:59 -0400

    Update shardingConnection.java

--- shardingConnection.java
+++ shardingConnection.java
@@ -1,25 +1,12 @@
 package com.bot;
 
 import javax.security.auth.login.LoginException;
-
-import com.bot.api.getQ;
 import com.bot.commands.CommandListener;
-import com.bot.reactions.ReactionListener;
-import com.not.bfd.GuidListener;
-
+import com.bot.guilds.GuildListener;
 import argo.saj.InvalidSyntaxException;
 import net.dv8tion.jda.core.entities.Game;
-
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import net.dv8tion.jda.bot.sharding.DefaultShardManager;
 import net.dv8tion.jda.bot.sharding.DefaultShardManagerBuilder;
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.Event;
-import net.dv8tion.jda.core.hooks.InterfacedEventManager;
 
 public class connection {
 	
@@ -28,7 +15,7 @@ public class connection {
 
     public static void main(String[] args) throws LoginException, IOException, InvalidSyntaxException {
         new DefaultShardManagerBuilder()
-        		.addEventListeners(new CommandListener(), new GuidListener())
+        		.addEventListeners(new CommandListener(), new GuildListener())
         		.setGame(game)
         		.setToken(token)
         		.build();
