3e52b888b3d74c72980c35a7a95410bcca58ec4b
James <42484571+smitteh1@users.noreply.github.com>
2018-11-03 23:16:16 -0400

    Update Challenge.java

--- src/com/bot/challenge/Challenge.java
+++ src/com/bot/challenge/Challenge.java
@@ -1,5 +1,20 @@
 package com.bot.challenge;
 
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
 public class Challenge {
 
+	User player1;
+	User player2;
+	JDA bot;
+	MessageChannel channel;
+	
+	public Challenge(User p1, User p2, MessageChannel ch, JDA self) {
+		player1 = p1;
+		player2 = p2;
+		channel = ch;
+		bot = self;
+	}
 }
