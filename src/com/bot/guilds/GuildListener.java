1671e9ad1011a7169da1e7081a9898bc46d5c54e
James <42484571+smitteh1@users.noreply.github.com>
2018-11-04 09:33:17 -0500

    Update GuildListener.java

--- src/com/bot/guilds/GuildListener.java
+++ src/com/bot/guilds/GuildListener.java
@@ -3,6 +3,7 @@ package com.bot.guilds;
 import java.io.IOException;
 
 import com.bot.botlist.BFD;
+import com.bot.botlist.DiscordBotsOrg;
 
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
@@ -17,6 +18,7 @@ public class GuildListener extends ListenerAdapter {
 		System.out.println("Guild Added : " + guildName);
 		try {
 			BFD bfd = new BFD(bot.getSelfUser().getId(), bot);
+			DiscordBotsOrg dbg = new DiscordBotsOrg(bot.getSelfUser().getId(), bot);
 		} catch (IOException e) {e.printStackTrace();}
 	}
 }
